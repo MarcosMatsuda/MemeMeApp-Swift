9208d7a85c1e321b0c27cdde282cf638027354e8
Marcos V. S. Matsuda <marcosv.s.matsuda@Marcoss-MacBook-Pro.local>
2018-11-16 01:42:13 -0200

    feat: activityViewController, add Meme Struct, refactored code

---
+++ MemeMev1.0/MemeMev1.0/Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  MemeMev1.0
+//
+//  Created by Marcos V. S. Matsuda on 16/11/18.
+//  Copyright © 2018 Marcos V. S. Matsuda. All rights reserved.
+//
+
+import UIKit
+
+// MARK: struct Meme
+struct Meme {
+    var topTextField: String
+    var bottomTextField: String
+    var originalImage: UIImageView
+    var memedImage: UIImage
+}
--- MemeMev1.0/MemeMev1.0/ViewController.swift
+++ MemeMev1.0/MemeMev1.0/ViewController.swift
@@ -8,15 +8,10 @@
 
 import UIKit
 
-// MARK: struct Meme
-struct Meme {
-    var topTextField: String
-    var bottomTextField: String
-    var originalImage: UIImageView
-    var memedImage: UIImageView
-}
-
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
+    
+    @IBOutlet weak var toolBarTop: UIToolbar!
+    @IBOutlet weak var toolBarBottom: UIToolbar!
     
     @IBOutlet weak var imagePickerView: UIImageView!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
@@ -26,6 +21,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     var textFieldTag: Int = 0
     
+    // MARK: Life Cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -33,10 +30,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         textFieldBotton.text = "BOTTOM"
         textFieldTop.delegate = self
         textFieldBotton.delegate = self
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
         
         let center = NotificationCenter.default
@@ -46,42 +44,60 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     override func viewWillDisappear(_ animated: Bool) {
-        
         super.viewWillDisappear(animated)
+        
         let center = NotificationCenter.default
         center.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
         center.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
         
     }
     
+    // MARK: Meme
+    
+    @IBAction func share(_ sender: Any) {
+        save()
+    }
+    
     func save() {
+        
+        let memedImage: UIImage = generateMemedImage()
+        
         // Create the meme
-        _ = Meme(topTextField: textFieldTop.text!, bottomTextField: textFieldBotton.text!, originalImage: imagePickerView!, memedImage: imagePickerView)
+//        let meme = Meme(topTextField: textFieldTop.text!, bottomTextField: textFieldBotton.text!, originalImage: imagePickerView, memedImage: memedImage)
+        
+        let activityItem: [AnyObject] = [memedImage as AnyObject]
+        
+        let activityViewController = UIActivityViewController(activityItems: activityItem , applicationActivities: nil)
+        activityViewController.popoverPresentationController?.sourceView = self.view
+        self.present(activityViewController, animated: true, completion: nil)
     }
     
-    func generateMemedImage() -> UIImage {
-        
-        // TODO: Hide toolbar and navbar
-        
-        // Render view to an image
-        UIGraphicsBeginImageContext(self.view.frame.size)
-        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
-        let memedImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
-        UIGraphicsEndImageContext()
-        
-        // TODO: Show toolbar and navbar
-        
-        return memedImage
-    }
-    
+    // Especifica se a Controller View prefere que a status bar seja ocultada ou mostrada.
     override var prefersStatusBarHidden: Bool{
         return true
     }
     
+    // MARK: TextField
+    
+    // textField deve começar a edição
     func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
         self.textFieldTag = textField.tag
         return true
     }
+    
+    /// textField começou a edição
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.tag == 0 { textFieldTop.text = "" }
+        if textField.tag == 1 { textFieldBotton.text = "" }
+    }
+    
+    // textField deve retornar
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        view.endEditing(true)
+        return true
+    }
+    
+    // MARK: KeyBoard
     
     @objc func keyboardWillShow(_ notification:Notification) {
         
@@ -102,6 +118,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         return keyboardHeight
     }
+    
+    // MARK: Image
     
     @IBAction func pickAnImageForAlbum(_ sender: Any) {
         
@@ -130,14 +148,22 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         dismiss(animated: true, completion: nil)
     }
     
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        if textField.tag == 0 { textFieldTop.text = "" }
-        if textField.tag == 1 { textFieldBotton.text = "" }
+    func generateMemedImage() -> UIImage {
+        
+        // TODO: Hide toolbar and navbar
+        self.toolBarTop.isHidden = true
+        self.toolBarBottom.isHidden = true
+        
+        // Render view to an image
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let memedImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        // TODO: Show toolbar and navbar
+        self.toolBarTop.isHidden = false
+        self.toolBarBottom.isHidden = false
+        
+        return memedImage
     }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        view.endEditing(true)
-        return true
-    }
-    
 }
